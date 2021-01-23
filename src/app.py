39f9909f32d1b7b6818b62699c35e7fc3f5b1368
adrianmrozo <72199535+adrianmrozo@users.noreply.github.com>
2021-01-23 02:33:28 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 # flask_ngrok_example.py
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 from test import test_one
 from test import test_new
 from main import model
@@ -21,17 +21,25 @@ def predict():
     output = "<h1>Welcome!</h1><br>Please find below an overview of the testing.<br><br>An image has been selected for you with the following image number out of the CIFAR 10 test dataset: " + str(number) + "<br><br>Please add in your browser URL '/yourimage' to see your test image, out of the CIFAR 10 test dataset." + "<br><br>The model predicted the following category of the picture: " + str(pred_label) + "<br><br>The following category is the correct one: " + str(test_label)
     return output
 
-@app.route("/enternumber")
-def enternumber():
+@app.route("/login", methods=["POST", "GET"])
+def login():
     if request.method == "POST":
         user = request.form["nm"]
         return redirect(url_for("user", usr=user))
     else:
-        return render_template("<form action='#' method='post'><p>Enter a number:</p><p><input type='number' name='nm' /></p></form>")
+        output = '<form action="#" method="post"><p>Name:</p><p><input type="text" name="nm" /></p><p><input type="submit" value="submit"/></p></form>'
+        return output
+    
+#@app.route("/<usr>")
+#def user(usr):
+#    return f"<h1>{usr}</h1>"
+
 
 @app.route("/<usr>")
 def user(usr):
-    return f"<h1>{usr}</h1>"
+    testData, test_label, pred_label, number = test_new(model, int(usr))
+    output = "<h1>Welcome!</h1><br>Please find below an overview of the testing.<br><br>An image has been selected for you (randomly) with the following image number out of the CIFAR 10 test dataset: " + str(number) + "<br><br>Please add in your browser URL '/yourimage' to see your test image, out of the CIFAR 10 test dataset." + "<br><br>The model predicted the following category of the picture: " + str(pred_label) + "<br><br>The following category is the correct one: " + str(test_label)
+    return output
 
 #not working:
 #@app.route("/enternumber")
