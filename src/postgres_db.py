e374dd73372ec8b3675b0200789d3e83af108590
adrianmrozo <72199535+adrianmrozo@users.noreply.github.com>
2021-01-25 01:44:28 +0100

    Update postgres_db.py

--- src/postgres_db.py
+++ src/postgres_db.py
@@ -5,9 +5,9 @@
 #password = input("Insert a password for your database:") or "pgpass"
 
 
-host = "127.0.0.1"
+host = "postgres"
 database = "milestone5"
-port = "5005"
+port = "5432"
 user = "postgres"
 password = "pgpass"
 
@@ -15,37 +15,103 @@ import psycopg2
 import numpy as np
 
 
+
 con = psycopg2.connect(dbname=database, user=user, password=password, host=host, port = port)
+
 cur = con.cursor()
 
 # create input data table
-cur.execute("CREATE TABLE input_data (ID SERIAL PRIMARY KEY, input_label TEXT, image TEXT);")
+#cur.execute("CREATE TABLE input_data (ID SERIAL PRIMARY KEY, input_label TEXT);")
 
+#create predictions table
+#cur.execute("CREATE TABLE predictions (ID SERIAL PRIMARY KEY, prediction TEXT);")
 
 #train the model and store it
 #also make it available in this script
-import main
-model = main.model
-
+#import main
+#model = main.model
 
 #store test data, test label, prediction label
-from test import test_one
-test_data, test_label, pred_label = test_one(model)
+#from test import test_one
+testData, test_label, pred_label = test_one(model)
+
+#cur.execute("CREATE TABLE input_data (ID SERIAL PRIMARY KEY, input_label varchar);")
+cur.execute("CREATE TABLE predictions (ID SERIAL PRIMARY KEY, prediction varchar);")
 
 
-#load testdata into database input_data
-cur.execute("insert into input_data (ID, input_label, image) values (%s, %s, %s)", (1, test_label, str(test_data)) )
+cur.execute('''
+CREATE SEQUENCE idsequence
+    start 10
+    increment 1;''')
+
 
 
 #execute query
-cur.execute("select * from input_data;")
-image1 = np.fromstring(cur.fetchall()[-1], dtype = int).reshape(32,32,3)
-print ("These are the inputs that have been tested so far:")
-print(cur.fetchall()[-1], image1)
+#cur.execute("select * from input_data;")
+#print ("These are the inputs that have been tested so far:")
+#print(cur.fetchall())
+
 
 #cur.execute("select * from predictions;")
 #print ("The CNN predicted the tested inputs to be:")
 #print(cur.fetchall())
+
+
 #commit data to db
 con.commit()
 con.close()
+
+
+#store test data, test label, prediction label
+
+def savingtestresult():
+#def savingtestresult(test_label, test_data, pred_label):
+
+    host = "postgres"
+    database = "milestone5"
+    port = "5432"
+    user = "postgres"
+    password = "pgpass"
+
+    con = psycopg2.connect(dbname=database, user=user, password=password, host=host, port = port)
+    cur = con.cursor()
+
+    #savingtestresult.counter += 1
+    #load testdata into database input_data
+    cur.execute("insert into predictions (ID, prediction) values (nextval('idsequence'), %s)", (pred_label,))
+
+    #execute query
+    #cur.execute("select * from input_data;")
+    #image1 = np.fromstring(cur.fetchall()[-1], dtype = int).reshape(32,32,3)
+    #print ("These are the inputs that have been tested so far:")
+    #print(cur.fetchall()[-1], image1)
+
+    #cur.execute("select * from predictions;")
+    #print ("The CNN predicted the tested inputs to be:")
+    #print(cur.fetchall())
+    #commit data to db
+    con.commit()
+    con.close()
+#savingtestresult.counter = 0
+
+def printdatabase():
+    host = "postgres"
+    database = "milestone5"
+    port = "5432"
+    user = "postgres"
+    password = "pgpass"
+
+    con = psycopg2.connect(dbname=database, user=user, password=password, host=host, port = port)
+
+    cur = con.cursor()
+    #my_table = pd.read_sql_table('table_name', connection)
+    my_table = pd.read_sql('select * from predictions', con)
+    #another_attempt= psql.read_sql("SELECT * FROM input_data", con)
+
+    return my_table
+
+    # OR
+    #print(another_attempt) 
+    con.close()
+
+
